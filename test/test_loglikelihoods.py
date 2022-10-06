d6cb795bfd599b4213e24f2e747db190c47dae7d
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2022-10-06 10:26:20 +0900

    [test] Add the test for logsumexp with weight=None

--- test/test_loglikelihoods.py
+++ test/test_loglikelihoods.py
@@ -15,6 +15,16 @@ class TestFuncs(unittest.TestCase):
         ans = np.array([26.90172323, 28.90172323, 30.90172323, 32.90172323])
         assert np.allclose(ans, ll)
 
+        ll = compute_config_loglikelihoods(basis_loglikelihoods=bll, weights=None) + np.log(weights[0])
+        assert ll.size == n_samples
+        assert np.allclose(ans, ll)
+
+        weights = np.arange(1, n_basis + 1).astype(np.float64)
+        weights /= np.sum(weights)
+        ll = compute_config_loglikelihoods(basis_loglikelihoods=bll, weights=None)
+        ans = np.array([28.00033552, 30.00033552, 32.00033552, 34.00033552])
+        assert np.allclose(ans, ll)
+
 
 if __name__ == "__main__":
     unittest.main()
